4495a8b8022639fbb0f20fbb3a5aabd22b310a33
fabros <frosato@outlook.com>
2017-06-21 18:35:43 +0200

    Adds debuggers detection models and checks.

--- pafish_models.py
+++ pafish_models.py
@@ -19,17 +19,92 @@ class SimStateParanoid(SimStatePlugin):
 
     def __init__(self):
         SimStatePlugin.__init__(self)
-
         self.tsc = 10**9  # random init tick count
+        self.last_error = 0  # should be thread-local, but whatever
 
     def copy(self):
         c = SimStateParanoid()
         c.tsc = self.tsc
-
+        c.last_error = self.last_error
         return c
 
 
 # HOOKS #
+
+# Debuggers detection
+
+class IsDebuggerPresent(simuvex.SimProcedure):
+
+    def execute(self, state, successors=None, arguments=None, ret_to=None):
+        super(IsDebuggerPresent, self).execute(state, successors, arguments, ret_to)
+        state.regs.esp += 4 * 0
+
+    def run(self):
+
+        self.argument_types = {}
+
+        self.return_type = simuvex.s_type.SimTypeInt()
+
+        ret_expr = 0  # always return false
+        print "IsDebuggerPresent: " + "=> " + str(ret_expr)
+        return ret_expr
+
+
+class SetLastError(simuvex.SimProcedure):
+
+    def execute(self, state, successors=None, arguments=None, ret_to=None):
+        super(SetLastError, self).execute(state, successors, arguments, ret_to)
+        state.regs.esp += 4 * 1
+
+    def run(self, dwErrCode):
+
+        self.argument_types = {
+            0: simuvex.s_type.SimTypeInt(),
+        }
+
+        self.return_type = None
+
+        self.state.paranoid.last_error = dwErrCode.args[0]
+
+        print "SetLastError: " + str(dwErrCode) + " " + "=> " + 'void'
+        return
+
+
+class GetLastError(simuvex.SimProcedure):
+
+    def execute(self, state, successors=None, arguments=None, ret_to=None):
+        super(GetLastError, self).execute(state, successors, arguments, ret_to)
+        state.regs.esp += 4 * 0
+
+    def run(self):
+
+        self.argument_types = {}
+
+        self.return_type = simuvex.s_type.SimTypeInt()
+
+        ret_expr = self.state.paranoid.last_error
+        print "GetLastError: " + "=> " + str(ret_expr)
+        return ret_expr
+
+
+class OutputDebugString(simuvex.SimProcedure):
+
+    def execute(self, state, successors=None, arguments=None, ret_to=None):
+        super(OutputDebugString, self).execute(state, successors, arguments, ret_to)
+        state.regs.esp += 4 * 1
+
+    def run(self, lpOutputString):
+
+        self.argument_types = {
+            0: self.ty_ptr(simuvex.s_type.SimTypeString()),
+            }
+
+        self.return_type = None
+
+        self.state.paranoid.last_error = 1284  # Update last error since debugger is not present
+
+        print "OutputDebugString: " + str(lpOutputString) + " " + "=> " + 'void'
+
 
 def rdtsc_hook(state):
     # print "GOTCHA!"
@@ -201,6 +276,12 @@ def rdtsc_monkey_patch():
 
 
 def hook_all(proj):
+    # Debuggers detection
+    proj.hook_symbol("IsDebuggerPresent", angr.Hook(IsDebuggerPresent))
+    proj.hook_symbol("SetLastError", angr.Hook(SetLastError))
+    proj.hook_symbol("GetLastError", angr.Hook(GetLastError))
+    proj.hook_symbol("OutputDebugStringA", angr.Hook(OutputDebugString))
+
     rdtsc_monkey_patch()
     proj.hook_symbol("GetTickCount", angr.Hook(GetTickCount))
     proj.hook_symbol("Sleep", angr.Hook(Sleep))
